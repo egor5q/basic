5651eccbd4f183bcb90a2250db9500c33c4ad021
egor5q <31898567+egor5q@users.noreply.github.com>
2018-07-13 13:34:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,8 +24,12 @@ skills=[]
 
 client1=os.environ['database']
 client=MongoClient(client1)
-db=client.mafia
+db=client.minigame
 users=db.users
+chats=db.chats
+
+rolelist=['wolf', 'gunner', 'berserk', 'nindza', 'cat', 'killer']
+
 
 
 
@@ -36,243 +40,113 @@ def medit(message_text,chat_id, message_id,reply_markup=None,parse_mode='Markdow
 
 
     
-    
 @bot.message_handler(commands=['start'])
 def start(m):
-    x=user.find_one({'id':m.from_user.id})
-    if x==None:      
-        user.insert_one(createuser(m.from_user.id))
-        print('Юзер создал аккаунт! Его имя: '+m.from_user.first_name)
-    x=m.text.split('/start')
-    if len(x)==2:
-       try:
-        if m.from_user.id==m.chat.id:
-         if m.from_user.id not in games[int(x[1])]['players']:
-          if len(games[int(x[1])]['players'])<10:
-           if int(x[1])<0:
-            i=0              
-            if games[int(x[1])]['play']==0:
-                games[int(x[1])]['players'].update(createuser(m.from_user.id, m.from_user.first_name))
-                text=''           
-                for ids in games[int(x[1])]['players']:
-                    if games[int(x[1])]['players'][ids]['id']==m.from_user.id:
-                        player=games[int(x[1])]['players'][ids]
-                bot.send_message(m.from_user.id, 'Вы успешно присоединились!')
-                b=0
-                for g in games[int(x[1])]['players']:
-                    text+=games[int(x[1])]['players'][g]['name']+'\n'
-                    b+=1
-                medit('Игроки: '+str(b)+'\n\n*'+text+'*', games[int(x[1])]['id'], games[int(x[1])]['users'])
-                games[int(x[1])]['userlist']+=text+'\n'
-                bot.send_message(games[int(x[1])]['id'], player['name']+' присоединился!')
-          else:
-            bot.send_message(m.from_user.id, 'Слишком много игроков! Мест не осталось!')
-       except:
-        if m.chat.id==m.from_user.id:
-            bot.send_message(m.from_user.id, 'Игра crossfire')
-
-            
-@bot.message_handler(commands=['extend']) 
-def extendd(m):
-    if m.chat.id in games:
-        if games[m.chat.id]['play']!=1:
-            if m.from_user.id in games[m.chat.id]['players']:
-                x=m.text.split('/extend')
-                if len(x)==2:
-                    try:
-                        if int(x[1])>=1:
-                            games[m.chat.id]['timebeforestart']+=int(x[1])
-                            if games[m.chat.id]['timebeforestart']>=300:
-                                games[m.chat.id]['timebeforestart']=300
-                                bot.send_message(m.chat.id, 'Время до начала перестрелки увеличено! Осталось 5 минут.')
-                            else:
-                                bot.send_message(m.chat.id, 'Время до начала перестрелки увеличено на '+x[1]+'! Осталось '+str(games[m.chat.id]['timebeforestart'])+' секунд.')
-                        else:
-                            x=bot.get_chat_administrators(m.chat.id)
-                            i=10
-                            for z in x:       
-                                if m.from_user.id==z.user.id:
-                                    i=1
-                                else:
-                                    if i!=1:
-                                        i=10
-                            if i==1:
-                                games[m.chat.id]['timebeforestart']+=int(x[1])
-                                a=x[1]
-                                if games[m.chat.id]['timebeforestart']<=0:
-                                    pass
-                                else:
-                                    bot.send_message(m.chat.id,'Время до начала перестрелки увеличено на '+a+'! Осталось '+str(games[m.chat.id]['timebeforestart'])+' секунд.')
-                            else:
-                                bot.send_message(m.chat.id, 'Только администратор может использовать эту команду!')
-                    except:
-                        games[m.chat.id]['timebeforestart']+=30
-                        if games[m.chat.id]['timebeforestart']>=300:
-                            games[m.chat.id]['timebeforestart']=300
-                        bot.send_message(m.chat.id, 'Время до начала перестрелки увеличено на 30! Осталось '+str(games[m.chat.id]['timebeforestart'])+' секунд.')
-                else:
-                    games[m.chat.id]['timebeforestart']+=30
-                    if games[m.chat.id]['timebeforestart']>=300:
-                            games[m.chat.id]['timebeforestart']=300
-                    bot.send_message(m.chat.id, 'Время до начала перестрелки увеличено на 30! Осталось '+str(games[m.chat.id]['timebeforestart'])+' секунд.')
-    
-            
-@bot.message_handler(commands=['flee'])
-def flee(m):
-    if m.chat.id in games:
-     if games[m.chat.id]['play']!=1:
-      if m.from_user.id in games[m.chat.id]['players']:
-        del games[m.chat.id]['players'][m.from_user.id]
-        text=''
-        for g in games[m.chat.id]['players']:
-            text+=games[m.chat.id]['players'][g]['name']+'\n'
-        bot.send_message(m.chat.id, m.from_user.first_name+' сбежал!')
-        medit('Игроки: \n\n*'+text+'*', m.chat.id, games[m.chat.id]['users'])
+  x=m.text.split('/start')
+  print(x)
   
-
-@bot.message_handler(commands=['players'])
-def playerss(m):
-    if m.chat.id in games:
-        bot.send_message(m.chat.id, 'Вот список игроков', reply_to_message_id=games[m.chat.id]['users'])
-
-            
-def secnd(id):
-    games[id]['timebeforestart']-=1
-    if games[id]['timebeforestart']<=0:
-        begin(id)
-    else:
-        Keyboard=types.InlineKeyboardMarkup()
-        Keyboard.add(types.InlineKeyboardButton(text='Присоединиться', url='telegram.me/crossfirebot?start='+str(id)))
-        if games[id]['timebeforestart']==180:
-            msg=bot.send_message(id, 'Осталось 3 минуты! Жмите "Присоединиться", чтобы поучаствовать в перестрелке!', reply_markup=Keyboard)
-            games[id]['todel'].append(msg.message_id)
-        elif games[id]['timebeforestart']==60:
-            msg=bot.send_message(id, 'Осталось 60 секунд! Жмите "Присоединиться", чтобы поучаствовать в перестрелке!', reply_markup=Keyboard)
-            games[id]['todel'].append(msg.message_id)
-        elif games[id]['timebeforestart']==30:
-            msg=bot.send_message(id, 'Осталось 30 секунд! Жмите "Присоединиться", чтобы поучаствовать в перестрелке!', reply_markup=Keyboard)
-            games[id]['todel'].append(msg.message_id)
-        elif games[id]['timebeforestart']==10:
-            msg=bot.send_message(id, 'Осталось 10 секунд! Жмите "Присоединиться", чтобы поучаствовать в перестрелке!', reply_markup=Keyboard)
-            games[id]['todel'].append(msg.message_id)
-        t=threading.Timer(1, secnd, args=[id])
-        t.start()
-            
-            
-@bot.message_handler(commands=['startgame'])
-def startgame(m):
-  if m.chat.id<0:
-    if m.chat.id not in games:
-        games.update(creategame(m.chat.id))  
-        tt=threading.Timer(1, secnd, args=[m.chat.id])
-        tt.start()
-        Keyboard=types.InlineKeyboardMarkup()
-        Keyboard.add(types.InlineKeyboardButton(text='Присоединиться', url='telegram.me/crossfirebot?start='+str(m.chat.id)))
-        msg=bot.send_message(m.chat.id, m.from_user.first_name+' Начал(а) игру! Жмите кнопку ниже, чтобы присоединиться', reply_markup=Keyboard)
-        msg2=bot.send_message(m.chat.id, 'Игроки:\n', parse_mode='markdown')
-        games[m.chat.id]['users']=msg2.message_id
-        for ids in games:
-            if games[ids]['id']==m.chat.id:
-                game=games[ids]
-        game['todel'].append(msg.message_id)
-    else:
-      if games[m.chat.id]['play']==0:
-        Keyboard=types.InlineKeyboardMarkup()
-        Keyboard.add(types.InlineKeyboardButton(text='Присоединиться', url='telegram.me/crossfirebot?start='+str(m.chat.id)))
-        msg=bot.send_message(m.chat.id, 'Игра уже запущена! Жмите "присоединиться"!', reply_markup=Keyboard)
-        for ids in games:
-            if games[ids]['id']==m.chat.id:
-                game=games[ids]
-        game['todel'].append(msg.message_id)
-  else:
-    bot.send_message(m.chat.id, 'Играть можно только в группах!')
-    
-   
-def begin(id):
-  if id in games:
-   if games[id]['play']==0:
-    if len(games[id]['players'])>=5:
-        for ids in games[id]['todel']:
-            try:
-                bot.delete_message(id, ids)
-            except:
-                pass
-        i=1
-        for ids in games[id]['players']:
-            games[id]['players'][ids]['number']=i
-            i+=1
-        bot.send_message(id, 'Игра начинается!')
-        games[id]['play']=1
-        xod(games[id])
-    else:
-        for ids in games[id]['todel']:
-            try:
-                bot.delete_message(id, ids)
-            except:
-                pass
-        bot.send_message(id, 'Недостаточно игроков!')
+  if users.find_one({'id':m.from_user.id})==None:
         try:
-            del games[id]
+            bot.send_message(m.from_user.id, 'Вы создали персонажа! Теперь дайте ему имя командой /name.')
+            users.insert_one(createuser(m.from_user.id, m.from_user.first_name))
+            x=random.choice(rolelist)
+            users.update_one({'id':m.from_user.id}, {'$set':{'role':x}})
+            bot.send_message(m.chat.id, 'Ваша первая роль - '+roletoname(x)+'.')
         except:
-            pass
+            bot.send_message(m.chat.id, 'Напишите боту в личку!')
 
-          
-          
-def xod(id):
-  rolelist=['gunner', 'volk']
-  for ids in games[id]['players']:
-        pass
-          
-          
-          
-          
-        
-@bot.message_handler(commands=['forcestart'])
-def forcem(m):
-  if m.chat.id in games:
-    i=0
-    x=bot.get_chat_administrators(m.chat.id)
-    for z in x:       
-        if m.from_user.id==z.user.id:
-           i=1
+@bot.message_handler(commands=['addme'])
+def addme(m):
+    x=chats.find_one({'id':m.chat.id})
+    if x!=None:
+      y=users.find_one({'id':m.from_user.id})
+      if y!=None:
+       if y['name']!=None:
+        if m.from_user.id not in x['users']:
+            chats.update_one({'id':m.chat.id}, {'$push':{'users':m.from_user.id}})
+            bot.send_message(m.chat.id, 'Теперь вы в игре!')
         else:
-            if i!=1:
-                i=10
-    if i==1:
-        if m.chat.id in games:
-            games[m.chat.id]['timebeforestart']=1
+            bot.send_message(m.chat.id, 'Вы уже в игре!')
+       else:
+         bot.send_message(m.chat.id, 'Сначала дайте персонажу имя!')
+      else:
+        bot.send_message(m.chat.id, 'Напишите боту в личку!')
+
+
+@bot.message_handler(commands=['fight'])
+def fight(m):
+    pass
+        
+        
+
+def roletoname(x):
+    role='У роли нет названия, пишите @Loshadkin'
+    if x=='wolf':
+        role='Волк'
+    if x=='gunner':
+        role='Стрелок'
+    if x=='berserk':
+        role='Берсерк'
+    if x=='nindza':
+        role='Ниндзя'
+    if x=='cat':
+        x='Кот'
+    if x=='killer':
+        role='Убийца'
+    return role
+
+
+@bot.message_handler(commands=['name'])
+def name(m):
+    text=m.text.split(' ')
+    if len(text)==2:
+        if len(text[1])<=25:
+          if '@' not in text[1]:
+            zz=users.find({})
+            spisok=[]
+            for ids in zz:
+                spisok.append(ids['name'])
+            if text[1] not in spisok:
+                x=users.find_one({'id':m.from_user.id})
+                users.update_one({'id':m.from_user.id}, {'$set':{'name':text[1]}})
+                bot.send_message(m.chat.id, 'Вы успешно изменили имя на '+text[1]+'!')
+            else:
+                bot.send_message(m.chat.id, 'Такое имя уже занято!')
+          else:
+            bot.send_message(m.chat.id, 'Нельзя использовать символ "@" в нике!')
+     else:
+            bot.send_message(m.chat.id, 'Длина ника не должна превышать 25 символов!')
     else:
-        bot.send_message(m.chat.id, 'Только администратор может использовать эту команду!')
-        
-        
+       bot.send_message(m.chat.id, 'Для переименования используйте формат:\n/name *имя*, где *имя* - имя вашего персонажа.', parse_mode='markdown')
 
 
- 
-def creategame(id):
-    return {id:{
-        'players':{},
-        'id':id,
-        'todel':[],
-        'toedit':[],
-        'play':0,
-        'timebeforestart':300,
-        'users':None,
-        'userlist':'Игроки:\n\n'
-    }
-           }
-
-
-def createuser(id):
+def createchat(id):
     return{'id':id,
-           'role':None,
-           'die':0
+           'users':[]
           }
+    
+    
+def createuser(id, name):
+    return{'id':id,
+           'nameofuser':name,
+           'name':None,
+           'role':None,
+           'dies':0,
+           'wins':0,
+           'looses':0,
+           'games':0
+          }
+
+@bot.message_handler(content_types=['text'])
+def textt(m):
+    x=chats.find_one({'id':m.chat.id})
+    if x==None:
+        chats.insert_one(createchat(m.chat.id))
   
   
 if True:
  try:
    print('7777')
-   bot.send_message(-1001208357368, 'Бот был перезагружен!')
    bot.polling(none_stop=True,timeout=600)
  except (requests.ReadTimeout):
         print('!!! READTIME OUT !!!')           
