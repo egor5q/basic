a836b1acfd78815e111c86f777a3d3addff963b2
egor5q <31898567+egor5q@users.noreply.github.com>
2018-07-15 22:29:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -84,7 +84,7 @@ def pokemon(m):
    if x!=None:
     y=random.choice(pokemons)
     if y not in x['pokemons']:
-        pokemonss.update_one({'id':m.from_user.id}, {'push':{'pokemons':y}})
+        pokemonss.update_one({'id':m.from_user.id}, {'$push':{'pokemons':y}})
     bot.send_message(m.chat.id, 'Вам выпал покемон *'+y+'*!', parse_mode='markdown')
     pokeban.append(m.from_user.id)
     t=threading.Timer(60, unpoke, args=[m.chat.id])
