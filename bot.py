dadec321066f35ab7e4fd2d0704663d4d99fe8f5
egor5q <31898567+egor5q@users.noreply.github.com>
2018-07-22 21:45:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -674,7 +674,7 @@ if True:
             bot.polling(none_stop=True,timeout=1)
             print('checkkk')
             check = False
-          except (requests.exceptions.ConnectionError):
+          except:
             time.sleep(1)
    
 #if __name__ == '__main__':
