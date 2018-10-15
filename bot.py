76955cecbb8aa52e4c9f776b778bb2d812831502
egor5q <31898567+egor5q@users.noreply.github.com>
2018-10-15 15:05:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,6 +37,28 @@ nearlocs={'spystart':['leftcorridor','rightcorridor','midcorridor'],
           'midcorridor':['spystart','treasure'],
           'stock':['rightpass','leftpass','treasure']
 }
+nearlocs2={
+'a1':['a2','b1'],
+    'a2':['a1','a3','b2'],
+    'a3':['a2','a4','b3'],
+    'a4':['a3','b4'],
+    'b1':['a1','b2','c1'],
+    'b2':['b1','a2','b3','c2'],
+    'b3':['b2','a3','b4','c3']
+    'b4':['b3','a4','c4'],
+    'c1':['b1','c2','d1'],
+    'c2':['c1','b2','c3','d2']
+    'c3':['c2','b3','c4','d3'],
+    'c4':['c3','b4','d4'],
+    'd1':['c1','d2','e1'],
+    'd2':['d1','c2','d3','e2'],
+    'd3':['d2','c3','d4','e3'],
+    'd4':['d3','c4','e4'],
+    'e1':['d1','e2'],
+    'e2':['e1','d2','e3'],
+    'e3':['e2','d3','e4'],
+    'e4':['e3','d4']
+}
 
 
 @bot.message_handler(commands=['stats'])
