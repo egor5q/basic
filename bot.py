82c93006b9e1b610ee737a8c639524ac04f744a8
egor5q <31898567+egor5q@users.noreply.github.com>
2019-03-16 00:42:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,8 +18,13 @@ db=client.
 users=db.users
 
 
+try:
+    pass
 
-if True:
-   print('7777')
-   bot.polling(none_stop=True,timeout=600)
+except Exception as e:
+    print('Ошибка:\n', traceback.format_exc())
+    bot.send_message(441399484, traceback.format_exc())
+
+print('7777')
+bot.polling(none_stop=True,timeout=600)
 
